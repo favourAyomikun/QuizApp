9939cfbcde656cd80a35d2514b300c0ec2786f23
Oshoaro Favour <oshoarofavour@gmail.com>
2023-09-11 18:57:03 +0100

    just worked on the next button

--- src/pages/QuizTemplate.jsx
+++ src/pages/QuizTemplate.jsx
@@ -3,9 +3,9 @@ import { quizData } from "../data/data";
 
 const QuizQuestions = () => {
   const [data, setData] = useState(quizData);
-  const [currentQuestionIndex, setCurrentQestion] = useState(0);
+  const [currentQuestionIndex, setCurrentQestionIndex] = useState(0);
   const [userAnswer, setUserAnswer] = useState(null);
-  const [score, userScore] = useState(0);
+  const [userScore, setUserScore] = useState(0);
   // const [score, setScore] = useState({
   //     score: 0,
   //     correctAnswers: 0,
@@ -16,26 +16,46 @@ const QuizQuestions = () => {
 
   // const question = setData[currentQuestionIndex]
   const presentQuestion = data[currentQuestionIndex];
-  console.log(presentQuestion);
+  // console.log(presentQuestion);
+
+  // console.log(currentQuestionIndex <= data.length)
+
+  const handleAnswerClick = (chosenOption) => {
+    setUserAnswer(chosenOption)
+
+    if(chosenOption === presentQuestion.correctAnswer) {
+      setUserScore(userScore + 5)
+      console.log(userScore);
+    }
+  }
+
+  const handleNextQuestion = () => {
+    if(currentQuestionIndex < data.length) {
+      setCurrentQestionIndex(currentQuestionIndex + 1)
+
+    } 
+  }
 
   return (
     <main className="container mx-auto bg-[#F4F3EE] rounded max-w-[50%] mt-10 pb-5 shadow-md shadow-[#8A817C]">
       <div>
         <p className=" ml-2 pt-2">Question {currentQuestionIndex + 1}:</p>
-        <h2 className="text-center font-bold text-[19px] mt-3 mb-5">
+        <h2 className="text-center font-bold text-lg mt-3 mb-5">
           {presentQuestion.question}
         </h2>
         <div className="flex flex-col items-center">
           {presentQuestion.options.map((option, index) => (
             <button
               key={index}
-              className="mb-7 w-[70%] rounded-sm bg-[#BCB8B1] text-base font-semibold"
+              className="mb-7 w-[70%] rounded-sm bg-[#BCB8B1] text-base font-semibold outline-none"
+              onClick={() => handleAnswerClick(option)}
+              
             >
               {option}
             </button>
           ))}
         </div>
-        <button className="bg-[#BCB8B1] shadow rounded-sm px-7 py-[2px] flex items-center mx-auto mt-5">
+        <button onClick={handleNextQuestion} className="bg-[#BCB8B1] outline-none shadow rounded-sm px-7 py-[2px] flex items-center mx-auto mt-5">
           <p className="font-semibold">Next</p>
         </button>
       </div>
