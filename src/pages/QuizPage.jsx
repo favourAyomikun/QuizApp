a3b5911e418dce056d66d70ef119adcc904e3615
Oshoaro Favour <oshoarofavour@gmail.com>
2023-09-11 16:07:09 +0100

    just finished the design of the quizquestion component

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -4,19 +4,23 @@ import QuizQuestions from "./QuizQuestions";
 
 const QuizPage = () => {
   const { inputValue, setInputValue } = useInputValue();
- 
-  useEffect(() => {
-    const savedInputValue = localStorage.getItem('inputValue')
 
-    if(savedInputValue) {
-      setInputValue(savedInputValue)
+  useEffect(() => {
+    const savedInputValue = localStorage.getItem("inputValue");
+
+    if (savedInputValue) {
+      setInputValue(savedInputValue);
     }
-  }, [])
+  }, []);
 
   return (
     <section>
-      <h2 className="uppercase pt-10 ml-10 font-bold text-2xl"> Welcome {inputValue}</h2>
-      <h3 className="text-center font-bold text-lg mt-5 tracking-wider">Answer the following questions:</h3>
+      <h2 className="uppercase pt-10 ml-10 font-bold text-2xl">
+        Welcome {inputValue}
+      </h2>
+      <h3 className="text-center font-bold text-lg mt-5 tracking-wider">
+        Answer the following questions:
+      </h3>
       <QuizQuestions />
     </section>
   );
--- src/pages/QuizQuestions.jsx
+++ src/pages/QuizQuestions.jsx
@@ -1,30 +1,46 @@
-import React, { useState } from 'react'
-import { quizData } from '../data/data'
+import React, { useState } from "react";
+import { quizData } from "../data/data";
 
 const QuizQuestions = () => {
-    const [data, setData] = useState(quizData)
-    const [currentQuestion, setCurrentQestion] = useState(0)
-    const [userAnswer, setUserAnswer] = useState(null)
-    const [score, userScore] = useState(0)
-    // const [score, setScore] = useState({
-    //     score: 0,
-    //     correctAnswers: 0,
-    //     wrongAnswers: 0
-    // })
+  const [data, setData] = useState(quizData);
+  const [currentQuestionIndex, setCurrentQestion] = useState(0);
+  const [userAnswer, setUserAnswer] = useState(null);
+  const [score, userScore] = useState(0);
+  // const [score, setScore] = useState({
+  //     score: 0,
+  //     correctAnswers: 0,
+  //     wrongAnswers: 0
+  // })
 
-    // console.log(quizData);
+  // console.log(quizData);
 
-    // const question = setData[currentQuestion]
-    const question = data[currentQuestion]
-    console.log(question);
-
-
+  // const question = setData[currentQuestionIndex]
+  const presentQuestion = data[currentQuestionIndex];
+  console.log(presentQuestion);
 
   return (
-    <main className='container'>
-        
+    <main className="container mx-auto bg-[#F4F3EE] rounded max-w-[50%] mt-10 pb-5 shadow-md shadow-[#8A817C]">
+      <div>
+        <p className=" ml-2 pt-2">Question {currentQuestionIndex + 1}:</p>
+        <h2 className="text-center font-bold text-[19px] mt-3 mb-4">
+          {presentQuestion.question}
+        </h2>
+        <div className="flex flex-col items-center">
+          {presentQuestion.options.map((option, index) => (
+            <button
+              key={index}
+              className="mb-7 w-[70%] rounded-sm bg-[#BCB8B1] text-base font-semibold"
+            >
+              {option}
+            </button>
+          ))}
+        </div>
+        <button className="bg-[#BCB8B1] shadow rounded-sm px-7 py-[2px] flex items-center mx-auto mt-5">
+          <p className="font-semibold">Next</p>
+        </button>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default QuizQuestions
+export default QuizQuestions;
