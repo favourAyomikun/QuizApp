f4f78d1655378e4867233f9bb6eb2ce54568ccd4
Oshoaro Favour <oshoarofavour@gmail.com>
2023-09-13 14:33:52 +0100

    made some changes to the countdownpage

--- src/pages/CountDown.jsx
+++ src/pages/CountDown.jsx
@@ -1,27 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Countdown = () => {
-  const [isCounting, setIsCounting] = useState(5);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const countdown = setInterval(() => {
-      if (isCounting > 0) {
-        setIsCounting(isCounting - 1);
-      } else {
-        clearInterval(countdown);
-        navigate('/quizPage')
-      }
-    }, 1000);
-
-    return () => {
-      clearInterval(countdown);
-    };
-  }, [isCounting]);
-
   return (
-    <div className="text-white flex flex-col justify-center h-screen text-center">
+    <div className="text-white flex flex-col justify-center items-center h-screen text-center">
       <div className="tracking-normal md:tracking-wide leading-10">
         <h1 className="text-[15px] md:text-xl font-bold underline">
           These are the following instructions:
@@ -31,7 +13,12 @@ const Countdown = () => {
         <p>3. The quiz has no time limit, Take your time.</p>
         <p>4. Score would be displayed once done with the questions.</p>
       </div>
-      <p className="text-base md:text-xl mt-5">{isCounting}</p>
+      <Link
+          to={'/quizPage'}
+          className="bg-white text-black text-sm md:text-base mt-6 h-8 w-[21%] md:w-[15%] lg:w-[10%] flex justify-center items-center shadow-md shadow-gray-800 rounded font-semibold"
+        >
+          <button>Continue</button>
+        </Link>
     </div>
   );
 };
