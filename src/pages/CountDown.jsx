1fe0330b764a9ddca73f9475c4fc117d6fcd2c5b
Oshoaro Favour <oshoarofavour@gmail.com>
2023-09-13 10:23:27 +0100

    worked on the responsiveness of the countdown page

--- src/pages/CountDown.jsx
+++ src/pages/CountDown.jsx
@@ -21,17 +21,17 @@ const Countdown = () => {
   }, [isCounting]);
 
   return (
-    <div className="text-white text-center pt-40 tracking-wide leading-10">
-      <div>
-        <h1 className="text-xl font-bold underline">
+    <div className="text-white flex flex-col items-center justify-center h-screen text-center">
+      <div className="tracking-normal md:tracking-wide leading-10">
+        <h1 className="text-[15px] md:text-xl font-bold underline">
           These are the following instructions:
         </h1>
-        <p>1. Answer all questions</p>
+        <p >1. Answer all questions</p>
         <p>2. Choose only one answer for the questions.</p>
         <p>3. The quiz has no time limit, Take your time.</p>
         <p>4. Score would be displayed once done with the questions.</p>
       </div>
-      <p className="text-xl mt-5">{isCounting}</p>
+      <p className="text-base md:text-xl mt-5">{isCounting}</p>
     </div>
   );
 };
