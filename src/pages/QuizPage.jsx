7a73030fbf3ef0af7c91d9bbd368be9040aae63e
Oshoaro Favour <oshoarofavour@gmail.com>
2023-09-13 11:33:11 +0100

    worked on the responsiveness of the quizpage component

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -19,9 +19,9 @@ const QuizPage = () => {
 
   return (
     <section>
-      <div className="flex items-center justify-between pt-10 px-10">
-        <h2 className="uppercase font-bold text-2xl text-white">Welcome {inputValue}</h2>
-        <button onClick={handleReset} className="bg-white shadow-md shadow-gray-800 hover:bg-[#022b3ac4] hover:text-white hover:shadow-transparent px-3 py-[2px] rounded">
+      <div className="flex items-center justify-between pt-10 px-5 md:px-10">
+        <h2 className="uppercase font-bold text-base md:text-xl lg:text-2xl text-white">Welcome {inputValue}</h2>
+        <button onClick={handleReset} className="text-[15px] md:text-base bg-white shadow-md shadow-gray-800 hover:bg-[#022b3ac4] hover:text-white hover:shadow-transparent px-3 py-[2px] rounded">
           RESET
         </button>
       </div>
