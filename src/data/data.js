9073bde71b95b1a2a2307315dcdac5a86cf5bf44
Oshoaro Favour <oshoarofavour@gmail.com>
2023-09-13 16:31:51 +0100

    made some changes

--- src/data/data.js
+++ src/data/data.js
@@ -3,60 +3,71 @@ export const quizData = [
         id: 1,
         question: 'Who won the last football world cup?',
         options: ['France', 'Nigeria', 'Argentina', 'England'],
-        correctAnswer: 'Argentina'
+        correctAnswer: 'Argentina',
+        pickedOption: ''
     },
     {
         id: 2,
         question: 'Who is the current president of Nigeria?',
         options: ['Muhammad Buhari', 'Goodluck Johnathan', 'Peter Obi', 'Bola Tinubu'],
-        correctAnswer: 'Bola Tinubu'
+        correctAnswer: 'Bola Tinubu',
+        pickedOption: ''
     },
     {
         id: 3,
         question: 'Who won the treble last season in football?',
         options: ['Chelsea', 'Manchester United', 'Newcastle', 'Man City'],
-        correctAnswer: 'Man City'
+        correctAnswer: 'Man City',
+        pickedOption: ''
     },
     {
         id: 4,
         question: 'Which country has the most world cup trophies?',
         options: ['England', 'France', 'Brazil', 'Argentina'],
-        correctAnswer: 'Brazil'
+        correctAnswer: 'Brazil',
+        pickedOption: ''
     },
     {
         id: 5,
         question: 'Compared to their body weight, what animal is the strongest?',
         options: ['Cow', 'Ant', 'Dung Beetle', 'Elephant'],
-        correctAnswer: 'Dung Beetle'
+        correctAnswer: 'Dung Beetle',
+        pickedOption: ''
     },
     {
         id: 6,
         question: 'How many dots appear on a pair of dice?',
         options: ['30', '42', '40', '20'],
-        correctAnswer: '42'
+        correctAnswer: '42',
+        pickedOption: ''
     },
     {
         id: 7,
         question: 'What colors is the flag of the United Nations?',
         options: ['Red & Purple', 'Green & White', 'Blue & White', 'Blue & Gray'],
-        correctAnswer: 'Blue & White'
+        correctAnswer: 'Blue & White',
+        pickedOption: ''
     },
     {
         id: 8,
         question: "What is Batman's butler called?",
         options: ['Mallorca', 'Alfred', 'Toby', 'Grayson'],
-        correctAnswer: 'Alfred'
+        correctAnswer: 'Alfred',
+        pickedOption: ''
     },
     {
         id: 9,
         question: 'What is the capital of New Zealand?',
         options: ['Washington Dc', 'Illinios', 'Wellington', 'Chicago'],
-        correctAnswer: 'Wellington'
+        correctAnswer: 'Wellington',
+        pickedOption: ''
+
     },
     {
         id: 10,
         question: "What is the name of Mackenzie Crook's character in sitcom The Office?",
         options: ['Gareth Keenan', 'Jim Halpert', 'Erin Hannon', 'Robert California'],
-        correctAnswer: 'Gareth Keenan'
+        correctAnswer: 'Gareth Keenan',
+        pickedOption: ''
     },
 ]
